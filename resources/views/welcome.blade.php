c1e21d4ae0571f66b1fd596634ac7ff5fe2fbe4e
yclgitc9 <yclgitc9@gmail.com>
2018-10-17 23:28:25 +0300

    2nd commit Changed Laravel to Lareavelxxxx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelxxxx
                 </div>
 
                 <div class="links">
