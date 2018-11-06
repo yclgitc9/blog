6aa1031398f7b2a4374c8f8c27115bce18ab4b4a
yclgitc9 <yclgitc9@gmail.com>
2018-11-06 11:35:23 +0300

    end of 09 final

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -12,7 +12,7 @@
                         <a href="post.html">
 
                             <img src="{{$post->image_url}}" alt=""> 
-                            <!-- <img src = "/img/Post_Image_3.jpg" alt=""/> -->
+                            
                         </a>
                     </div>
                     <div class="post-item-body">
