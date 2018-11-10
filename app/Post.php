27c1c51546e5e0f50c678d61c55ed469ae1dbc28
yclgitc9 <yclgitc9@gmail.com>
2018-11-10 14:25:49 +0300

    mdf111

--- app/Post.php
+++ app/Post.php
@@ -33,15 +33,15 @@ Class Post extends Model
         return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();        
     }
 
-    public function getBodyHtmlAttribute($value)
-    {
-        return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;
-    }
+    // public function getBodyHtmlAttribute($value)
+    // {
+    //     return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;
+    // }
 
-    public function getExcerptHtmlAttribute($value)
-    {
-        return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;
-    }
+    // public function getExcerptHtmlAttribute($value)
+    // {
+    //     return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;
+    // }
 
     public function scopeLatestFirst($query)
     {
