914b25d52d789469f3892fdd19085d22c6c0e10e
yclgitc9 <yclgitc9@gmail.com>
2018-11-10 16:15:07 +0300

    end of 016 created post categories

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
-// use GrahamCampbell\Markdown\Facades\Markdown;
 
 
 Class Post extends Model
@@ -32,16 +31,6 @@ Class Post extends Model
     {
         return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();        
     }
-
-    // public function getBodyHtmlAttribute($value)
-    // {
-    //     return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;
-    // }
-
-    // public function getExcerptHtmlAttribute($value)
-    // {
-    //     return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;
-    // }
 
     public function scopeLatestFirst($query)
     {
