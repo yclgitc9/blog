0e8c05a19f1d7d9840b48197a66964f838e6d960
yclgitc9 <yclgitc9@gmail.com>
2018-11-10 12:14:31 +0300

    markdown fix 02

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
-use GrahamCampbell\Markdown\Facades\Markdown;
+// use GrahamCampbell\Markdown\Facades\Markdown;
 
 class Post extends Model
 {
@@ -38,12 +38,12 @@ public function author()
 
     public function getBodyHtmlAttribute($value)
     {
-        return $this->body ? Markdown::convertToHtml(e($this->body)) : NULL;
+        return $this->body ? GrahamCampbell\Markdown\Facades\Markdown::convertToHtml(e($this->body)) : NULL;
     }
 
     public function getExcerptHtmlAttribute($value)
     {
-        return $this->excerpt ? Markdown::convertToHtml(e($this->excerpt)) : NULL;
+        return $this->excerpt ? GrahamCampbell\Markdown\Facades\Markdown::convertToHtml(e($this->excerpt)) : NULL;
     }
 
     public function scopeLatestFirst($query)
