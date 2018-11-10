b3853c13de963f307d7ccb1a899a88979b67af08
yclgitc9 <yclgitc9@gmail.com>
2018-11-10 13:11:00 +0300

    markdown fix 07

--- app/Post_original.php
+++ app/Post_original.php
@@ -2,33 +2,39 @@
 
 namespace App;
 
+use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
-use GrahamCampbell\Markdown\Facades\Markdown;
 
-Class Post extends Model
+
+class Post extends Model
 {
-    protected $dates = ['published_at'];
+    protected $dates  = ['published_at'];
 
     public function author()
     {
         return $this->belongsTo(User::class);
     }
 
+
     public function getImageUrlAttribute($value)
     {
         $imageUrl = "";
-        if (!is_null($this-image))
+        
+        if (! is_null($this->image))
         {
             $imagePath = public_path() . "/img/" . $this->image;
-            if (file_exists($imagePath)) $imageUrl = asset("img") . "/" . $this->image;            
-        }
+
+
+            if (file_exists($imagePath)) $imageUrl = asset("img") . "/" . $this->image;
+            
+        }        
         return $imageUrl;
     }
 
     public function getDateAttribute()
     {
-        return is_null($this-published_at) ? '' : $this->published_at->diffForHumans();        
+        return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
     }
 
     public function getBodyHtmlAttribute($value)
