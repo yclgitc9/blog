a4b3097dff49998f4cab7c88a574488d1a4f0f40
yclgitc9 <yclgitc9@gmail.com>
2018-11-11 16:35:51 +0300

    end of 018 part 2 filter posts by category part2

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,9 +13,9 @@ class BlogController extends Controller
 
     public function index() 
     {
-        $categories = Category::with([ 'posts' => function($query) {
-            $query->published();
-        }])->orderBy('title', 'asc')->get();
+        // $categories = Category::with([ 'posts' => function($query) {
+        //     $query->published();
+        // }])->orderBy('title', 'asc')->get();
         // die($categories);
 
         $posts = Post::with('author')
@@ -23,29 +23,36 @@ class BlogController extends Controller
                         ->published()
                         ->simplePaginate($this->limit);
         
-        return view("blog.index", compact('posts', 'categories')); 
+        return view("blog.index", compact('posts')); 
     }
 
-    public function category($id) 
+    public function category(Category $category) 
     {
-        $categories = Category::with(['posts' => function($query) {
-            $query->published();
-        }])->orderBy('title', 'asc')->get();
+        $categoryName = $category->title;
+
+
+        // $categories = Category::with(['posts' => function($query) {
+        //     $query->published();
+        // }])->orderBy('title', 'asc')->get();
         // die($categories);
 
-        $posts = Post::with('author')
-                        ->latestFirst()
-                        ->published()
-                        ->where('category_id', $id)
-                        ->simplePaginate($this->limit);
+        // $posts = Post::with('author')
+        //                 ->latestFirst()
+        //                 ->published()
+        //                 ->where('category_id', $id)
+        //                 ->simplePaginate($this->limit);
+
+        $posts = $category->posts()
+                            ->with('author')
+                            ->latestFirst()
+                            ->published()
+                            ->simplePaginate($this->limit);
         
-        return view("blog.index", compact('posts', 'categories')); 
+        return view("blog.index", compact('posts', 'categoryName')); 
     }
 
     public function show(Post $post)
     {
-        // die("show");
-        // $post = Post::published()->findOrFail($id);
         return view("blog.show", compact('post'));
     }
 
