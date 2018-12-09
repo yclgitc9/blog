ef26234c14f64d9b9008a2ea98898c2619ab63aa
yclmrl <yclgitc9@gmail.com>
2018-12-09 03:43:13 +0300

    end of 021 display popular posts

--- app/Post.php
+++ app/Post.php
@@ -43,6 +43,11 @@ Class Post extends Model
         return $query->orderBy('created_at', 'desc');
     }
 
+    public function scopePopular($query)
+    {
+        return $query->orderBy('view_count', 'desc');
+    }
+
     public function scopePublished($query)
     {
         return $query->where("published_at", "<=", Carbon::now());
